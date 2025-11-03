10d16753452cf0a135fbedf41f6cca9825189976
Shamshodbek2011 <assalom19865207mail.com@gmail.com>
2025-11-03 17:47:24 +0500

    php arrays

--- array.php
+++ array.php
@@ -1,32 +1,40 @@
 <?php 
-$sonlar = [2,5,7,34,24];
-function kattasonlar($massiv){
- $max = $massiv[0];
-    foreach($massiv as $value){
-            if($value > $max){
-                $max = $value ;
-            }
-            
-    }
-    return $max ;
+// Birinchi usul
+
+$sonlar = array(1,23,75,13,278);
+// Ikkinchi sonlar
+
+$sonlar1 = [1,34,345,334,233];
+print_r($sonlar);
+echo $sonlar[2];
+$cars = array("Volvo","Bmw","Mers");
+var_dump($cars);
+// Massiv ichidagi qiymatni o'zgARTIRISH
+
+echo $sonlar1[5];
+echo "\n";
+$sonlar1[5] = 100;
+echo $sonlar1[5];
+print_r($sonlar1);
+
+// add value 
+$cars = array("Volva","BMW","Tayota");
+foreach($cars as $x) {
+    echo "$x ";
 }
-echo kattasonlar($sonlar);
-
-
-echo"\n";
-
-
-
-
-
-
-
-
-
-
-
-
-
+array_push($cars, "Chevrolet");
+$cars[] = "BYD";
+print_r($cars);
+//werty
+$user = ["ism" =>"Ramazon","Familiya" => "Vapoyev", "t_yili" => "2011"];
+$son = [0=> 2 ,1 =>5];
+print_r($user);
+echo $user['ism'];
+print_r($son);
+$user['Familiya']= "Karimov";
+echo $user['Familiya'];
+$user['tel'] = 77777777777;
+print_r( $user) ;
 
 
 
